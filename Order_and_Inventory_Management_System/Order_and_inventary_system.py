3ce2229be6ecc685c9f0159573b7d609b5b33f9a
sanchesec <gustavo.sanches.ec@gmail.com>
2026-09-22 00:48:29 -0300

    feat: implement create order logic

--- Order_and_Inventory_Management_System/Order_and_inventary_system.py
+++ Order_and_Inventory_Management_System/Order_and_inventary_system.py
@@ -1,3 +1,4 @@
+
 products = [
     {"id": 101, "name": "Keyboard", "price": 150.00, "stock": 5},
     {"id": 102, "name": "Mouse", "price": 80.00, "stock": 8},
@@ -30,50 +31,91 @@ elif option in ("2","2-createorder"):
             print("Insert only positive inteer numbers")
             continue
         
-        else:
-            idorder = int(idorder)
+        
+        idorder = int(idorder)
 
         found = False
-
+        found_product = None
         for product in products:
 
             if product["id"] == idorder:
                 found = True
+                found_product = product
+                break
 
 
         if not found:
             print("Product not found, try again...")
+            continue
+        
+        print(f"Product found, ({found_product['name']})")
 
-        else:
-            print(f"Product found")
+        while True:
+
             stockorder = input("Inform the quantity of product: ").strip()
 
             if not stockorder.isdigit():
                 print("Insert only positive inteer numbers")
+                continue
+
+            stockorder = int(stockorder)
+        
+            if stockorder > found_product['stock']:
+                print(f"Sorry, but we have only {found_product['stock']} unitys in stock. Please try again")
+                continue
+
+            elif stockorder == 0:
+                print("Insert a quanty greater than zero...")
+                continue
 
             else:
-                stockorder = int(stockorder)
-                indexid = 0
+                print("Sucess!")
+                print(f"{stockorder} unitys of {found_product['name']} went into your cart")
+                break
 
-                for index, product in enumerate(products):
-                    if product["id"] == idorder:
-                        indexid = index
+        order = {"id": idorder, "name": found_product["name"],"price": found_product["price"], "quantity": stockorder }
+        orderclient.append(order)
 
-                if stockorder > products[indexid]["stock"]:
-                    print(f"Sorry, but we have only {products[indexid]['stock']} unitys in stock. Please try again")
+        again = input("You want to continue? Yes/No: ").strip().lower()
+
+        while again not in ("yes","y","no","n"):
+            print("Insert 'Yes' to add a new product in your order or 'not' for exit") 
+            again = input("You want to continue? Yes/No: ").strip().lower()
+             
+        if again in ("yes","y"):
+            continue
+        
+        elif again in ("no","n"):
+            print(f"Your order: {orderclient}")
+            while True:
+
+                confirm = input("Do you confirm your order? Yes/No: ").strip().lower()
+                if confirm not in ("yes","y","no","n"):
+                    print("Insert 'Yes' to confirm the order or 'not' for cancel the order")
+
+                    continue
+
+                elif confirm in ("yes","y"):
+                    print("Order successfully generated!")
+
+                    for product1 in products:
+                        for product2 in orderclient:
+                            if product2["id"] == product1["id"]:
+                                newstock = product1["stock"] - product2["quantity"] 
+                                product1["quantity"] = newstock
+                                break
+
+                    print(products)
+                        
+                    break
 
                 else:
-                    print("Sucess!")
-                    orderclient['id'] = idorder
-                    orderclient['unity'] = stockorder
+                    del orderclient
+                    print("Order successfully cancelled!")
+                    break
+            break
 
-                    again = input("You want to continue?: ").strip().lower()
-                    if again in ("no","n"):
-
-                        print(orderclient)
-                         
-                        break
-
+                                   
 
 
 
